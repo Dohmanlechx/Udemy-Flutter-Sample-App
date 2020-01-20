d2ac0d3b2a12cc1c25d2dcc68c1f63a2e8649956
David Öhman <david.ohman@apegroup.com>
2020-01-20 17:23:29 +0100

    -

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final questions = [
+      "What's your fav color?",
+      "What's your fav animal?",
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("First Flutter app"),
         ),
-        body: Text("Hey I'm here at Apegroup!"),
+        body: Column(
+          children: [
+            Text("The question!"),
+            RaisedButton(child: Text("Answer 1"), onPressed: null),
+            RaisedButton(child: Text("Answer 2"), onPressed: null),
+            RaisedButton(child: Text("Answer 3"), onPressed: null),
+          ],
+        ),
       ),
     );
   }

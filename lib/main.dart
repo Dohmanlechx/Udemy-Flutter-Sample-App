5127dad143b22321808257d1aee902f9d261bfe4
David Öhman <david.ohman@apegroup.com>
2020-01-21 10:15:55 +0100

    -

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print("Answer chosen!");
+    questionIndex += 1;
+    print(questionIndex);
   }
 
   @override
@@ -21,10 +31,10 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text("The question!"),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text("Answer 1"),
-              onPressed: () => print("Answer 1 chosen!"),
+              onPressed: () => answerQuestion,
             ),
             RaisedButton(
               child: Text("Answer 2"),

e3af54bbfaa6d8cfb3c1b3bf8475b6ef9c9902af
David Öhman <david.ohman@apegroup.com>
2020-01-23 11:59:00 +0100

    -

--- lib/main.dart
+++ lib/main.dart
@@ -13,39 +13,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final List<Map<String, Object>> _questions = const [
+    {
+      "questionText": "What's your favorite color?",
+      "answers": ["Black", "Purple", "Red", "Blue"],
+    },
+    {
+      "questionText": "What's your favorite animal?",
+      "answers": ["Dog", "Cat", "Elephant", "Horse", "Cow"],
+    },
+    {
+      "questionText": "Who's your favorite instructor?",
+      "answers": ["Max", "Max", "Max", "Max", "Max"],
+    }
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
-    setState(() {
-      if (_questionIndex == 2) {
-        _questionIndex = 0;
-      } else {
+    if (_questionIndex < _questions.length - 1) {
+      setState(() {
         _questionIndex += 1;
-      }
-    });
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final _questions = [
-      {
-        "questionText": "What's your favorite color?",
-        "answers": ["Black", "Purple", "Red", "Blue"],
-      },
-      {
-        "questionText": "What's your favorite animal?",
-        "answers": ["Dog", "Cat", "Elephant", "Horse", "Cow"],
-      },
-      {
-        "questionText": "Who's your favorite instructor?",
-        "answers": ["Max", "Max", "Max", "Max", "Max"],
-      }
-    ];
+    final Question question =
+        Question(_questions[_questionIndex]["questionText"]);
 
-    final question = Question(_questions[_questionIndex]["questionText"]);
-
-    final answers = (_questions[_questionIndex]["answers"] as List<String>)
-        .map((answerText) {
+    final List<Answer> answers =
+        (_questions[_questionIndex]["answers"] as List<String>)
+            .map((answerText) {
       return Answer(answerText, _answerQuestion);
     }).toList();
 
